79de1e225edd1b7e261b8a3109bb17d154009896
Dan Leaver <dleaver@gmail.com>
2020-06-30 21:05:57 -0600

    added some validations and all associations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,12 @@
 class Picture < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  has many :picture_comments, through :users, dependent: :destroy
+  has many :collections, through :collection_pictures
+
+  validates :url, presence: true
+  validates :title, presence: true
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many: :pictures, dependent: :destroy
+  has_many: :collections, dependent: :destroy
+  has_many: :picture_comments, through :pictures
+  #Should picture comments stay after user has been deleted? 
+  #this will leave them up.
+
+  #Devise handles validations here well enough?
+
+
 end
