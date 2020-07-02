73a7f17d9aa102bb6805541015731d00940a53af
Dan Leaver <dleaver@gmail.com>
2020-07-01 18:38:36 -0600

    typos galore

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,6 +1,6 @@
 class Collection < ApplicationRecord
   belongs_to :user
-  has_many pictures, through: :collection_pictures
+  has_many :pictures, through: :collection_pictures
   validates :title, presence: true
   
 end
