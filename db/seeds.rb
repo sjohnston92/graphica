7bd88201c0d89219bbbde2a3eae76d5523fb9196
Dan Leaver <dleaver@gmail.com>
2020-07-01 21:37:47 -0600

    seeds initial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require Rails.root.join("data", "urls.rb")
 
 
-# IMAGINE SEEDS ARE BEING MADE!!!!!
+7.times do
+  User.create(email: Faker::Internet.email, password: 'password', password_confirmation: 'password')
+  # User.create(email: "sarah@gmail.com", password: 'password', password_confirmation: 'password')
+  # User.create(email: "lee@gmail.com", password: 'password', password_confirmation: 'password')
+  # User.create(email: "pat@gmail.com", password: 'password', password_confirmation: 'password')
+  # User.create(email: "sally@gmail.com", password: 'password', password_confirmation: 'password')
+  # User.create(email: "bill@gmail.com", password: 'password', password_confirmation: 'password')
+end
+7.times do
+  Collection.create(user_id: (rand(6)+1), title: Faker::Movies::HarryPotter.location)
+end
+
+
+Category.create(title: "Animals") #1
+Category.create(title: "Art") #2
+Category.create(title: "Other") #3
+Category.create(title: "Nature") #4
+Category.create(title: "Places") #5
+Category.create(title: "People") #6
+Category.create(title: "Technology") #7
+
+
+IMAGE_URLS.map { |url| 
+
+  Picture.create(
+    category_id: (rand(6)+1), user_id: (rand(6)+1), title: Faker::Movies::StarWars.planet, 
+    url: url
+  )
+
+}
+
+
+
+50.times do
+  CollectionPicture.create(collection_id: (rand(6)+1), picture_id: (rand(26)+1))
+end 
+
+
+  50.times do
+    
+    PictureComment.create(user_id: (rand(6)+1), picture_id: (rand(26)+1), body: Faker::TvShows::DumbAndDumber.quote)
+    # puts Faker::TvShows::DumbAndDumber.quote
+  end
+  
+  puts 'data seeded!'
