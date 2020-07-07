ca3afbad16cfba7dfd4f80e8b9a88b4a690886d8
Dan Leaver <dleaver@gmail.com>
2020-07-07 12:15:16 -0600

    tidy-up

--- client/src/components/modal/ShowModal.js
+++ client/src/components/modal/ShowModal.js
@@ -1,31 +1,26 @@
 import React, { useState } from 'react'
 import PictureModal from './PictureModal'
-import Feed from '../home/Feed'
+import Test from './Test'
 
 const ShowModal = () => {
-
   const [show, setShow] = useState(false);
-  const children = ["This is where you can put content in the modal"]
-    const toggle = () => { 
-    setShow(!show)
-  }
+  const children = [ <Test />]
+  const toggle = () => { setShow(!show) }
   const open = true
   
   const handleClick = (e) => {
-    e.preventDefault()
+    e.preventDefault() //Do I need this line?
     setShow(!show) 
   }
-
-
-
-
 
   return (
     <>
     <button onClick={handleClick}>Click to add Pic</button>
-  {show ? <div><PictureModal children={children} toggle={toggle} open={open} />
-</div> : ""}
-    
+      {show ? 
+        <div> 
+        <PictureModal children={children} toggle={toggle} open={open} />
+        </div>
+        : ""}
     </>
   )
 }
---
+++ client/src/components/modal/Test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Test = () => (
+  <>
+  <p>Select A Picture to Upload</p>
+  <input type="form"></input>
+  </>
+)
+
+export default Test
