a5189c81eebcf08a907d25d0128f058ae20de4fc
Braden Church <bradenchurch@gmail.com>
2020-07-07 19:34:42 -0600

    removed node modules and refactored locked files

--- client/src/components/profile/ProfileNavbar.js
+++ client/src/components/profile/ProfileNavbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProfileNavbar = () => (
   <div>
-  <a href="/">recent</a> <a href="/collection">collection</a> <a href="/">favorites</a> <a href="/" >settings</a>
+  <a href="/">recent</a> <a href="/collection">collection</a> <a href="/">favorites</a> <a href="/">settings</a>
   </div>
 )
 
