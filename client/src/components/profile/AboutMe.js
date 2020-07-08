b6ee823bae02e75f927b1bc20d6b0507780f70ae
Braden Church <bradenchurch@gmail.com>
2020-07-07 19:45:46 -0600

    fixed issues on AboutMe

--- client/src/components/profile/AboutMe.js
+++ client/src/components/profile/AboutMe.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Userinfo = () => (
-
-  <AboutMe type="textfield" name="bioName" placeholder="YOUR INFO HERE, HELLO From ABOUTME.JS"/>
-  
-  )
-
-
-
-
-
+const UserInfo = () => (
+  <AboutMe 
+    type="textfield" 
+    name="bioName" 
+    placeholder="YOUR INFO HERE, HELLO From ABOUTME.JS"
+  />
+)
   // about me under profile picture
-
 const AboutMe = styled.textarea`
-border: black;
-
-height: 15vh;
-
-width: 525px;
-box-sizing: border-box;
-outline: none
+  border: black;
+  height: 15vh;
+  width: 525px;
+  box-sizing: border-box;
 `
 
-export default AboutMe;
+export default UserInfo;
