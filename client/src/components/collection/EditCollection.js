519c96a051ae91c0f3438704b4e49ee7cd2ec9c8
Braden Church <bradenchurch@gmail.com>
2020-07-28 18:15:58 -0600

    prepping merge

--- client/src/components/collection/EditCollection.js
+++ client/src/components/collection/EditCollection.js
@@ -37,7 +37,9 @@ const EditCollection = (props) => {
                   </EditButton> 
                 <DeleteButton onClick={deleteCollection}>
                   Delete Collection
-
+                  <svg width="1rem" height="1rem" viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M14.5 3H9.5L8.5 4H5V6H19V4H15.5L14.5 3ZM16 9V19H8V9H16ZM6 7H18V19C18 20.1 17.1 21 16 21H8C6.9 21 6 20.1 6 19V7Z" fill="white"/>
+                  </svg>
                   </DeleteButton>
               </ButtonWrapper>
                 
