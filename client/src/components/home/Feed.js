a6eb002f505a838006302cbd94fc555ca8cea392
Dan Leaver <dleaver@gmail.com>
2020-08-16 21:49:58 -0600

    last 3 pictures in feed are assigned by height

--- client/src/components/home/Feed.js
+++ client/src/components/home/Feed.js
@@ -38,37 +38,68 @@ const Feed = (props) => {
     }
   }
 
-  const renderColumns = () => {
+  const renderColumns = (input, offset) => {
     const column_arrays = [[], [], []];
-    const column_height = [0,0,0]
-
-    props.pictures.forEach((listItem) => {
-      if (column_height[0] <= (column_height[1] && column_height[2])) {
+    const column_height = [0,0,0];
+    
+    const assignColumns = (listItem) => {
+      if (column_height[0] <= column_height[1] && column_height[0] <= column_height[2]) {
         column_arrays[0].push(listItem);
-        column_height[0] = column_height[0] + 1/listItem.ratio
-      } else if (column_height[1] <= (column_height[0] && column_height[2])) {
+        column_height[0] = column_height[0] + 1/listItem.ratio + offset
+      } else if (column_height[1] <= column_height[0] && column_height[1] <= column_height[2]) {
         column_arrays[1].push(listItem);
-        column_height[1] = column_height[1] + 1/listItem.ratio
+        column_height[1] = column_height[1] + 1/listItem.ratio + offset
       } else {
         column_arrays[2].push(listItem);
-        column_height[2] = column_height[2] + 1/listItem.ratio
+        column_height[2] = column_height[2] + 1/listItem.ratio + offset
       }
-    })
-  
-  const updateFeedState = (incomingId) => {
-    props.deletePicture(incomingId)
-  }
+    }
+    
+    const sortMany = () => {
+      let last3 = [];
+      let i = 1;
+
+      input.forEach((listItem) => {
+        if (i + 2 < input.length) {
+          assignColumns(listItem)
+        } else if (i + 2 === input.length) {
+          last3.push(listItem)
+        } else if (i + 1 === input.length) {
+          last3.push(listItem)
+        } else {
+          last3.push(listItem)
+          last3.sort((a,b) => a.ratio - b.ratio )
+          last3.forEach((listItem) => {
+            assignColumns(listItem)
+          })
+        }
+        i ++ 
+      })
+    }
+
+    if (input.length > 2){
+      sortMany()
+    } else if (input.length === 2) {
+      if (input[0].ratio < input[1].ratio) {
+        column_arrays[0].push(input[0])
+        column_arrays[1].push(input[1])
+      } else {
+        column_arrays[0].push(input[1])
+        column_arrays[1].push(input[0])
+      }
+    } else input.forEach(listItem => column_arrays[0].push(listItem));
+
     return (
       <>
         <FeedDiv>
           <ColumnContainer>
-            {column_arrays[0].map(listItem =><><Card key={listItem.id} image={listItem} updateFeedState={updateFeedState}/></>)}
+            {column_arrays[0].map(listItem =><><Card key={listItem.id} image={listItem} updateFeedState={props.deletePicture}/></>)}
           </ColumnContainer>
           <ColumnContainer>
-            {column_arrays[1].map(listItem =><><Card key={listItem.id} image={listItem} updateFeedState={updateFeedState}/></>)}
+            {column_arrays[1].map(listItem =><><Card key={listItem.id} image={listItem} updateFeedState={props.deletePicture}/></>)}
           </ColumnContainer>
           <ColumnContainer>
-            {column_arrays[2].map(listItem =><><Card key={listItem.id} image={listItem} updateFeedState={updateFeedState}/></>)}
+            {column_arrays[2].map(listItem =><><Card key={listItem.id} image={listItem} updateFeedState={props.deletePicture}/></>)}
           </ColumnContainer>
         </FeedDiv>
         <NoContent>
@@ -80,7 +111,7 @@ const Feed = (props) => {
     )
   }
 
-  return renderColumns();
+  return renderColumns(props.pictures, 0.15);
 };
 
 const NoContent = styled.div`
